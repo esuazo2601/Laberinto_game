0e7db3562eda87ce580ed4c632c90ab725e42cdf
esuazo2601 <esteban.suazo.m2601@gmail.com>
2024-04-21 13:16:24 -0400

    update

--- algorithms.py
+++ algorithms.py
@@ -1,28 +1,37 @@
 from objects import matrix
 
-def DFS(x,y,matrix:matrix):  
-  if x<0 or y<0 or x>=matrix.n or y >= matrix.m or matrix.visited[x][y]:
-    return 0
-  
-  matrix.increaseSteps()
+def DFS(x, y, matrix):
+    if x < 0 or y < 0 or x >= matrix.n or y >= matrix.m or matrix.visited[x][y]:
+        return 0
+    
+    if matrix.data[x][y] == 0:
+        return matrix.getSteps() + 1
 
-  matrix.setVisitedTrue(x,y)
-  #Moverse a la derecha
-  DFS(x = x+matrix.data[x][y], y=y, matrix = matrix)
-  #Moverse a la izquierda
-  DFS(x = x-matrix.data[x][y], y=y, matrix = matrix)
-  #Moverse arriba
-  DFS(x = x, y = y + matrix.data[x][y], matrix = matrix)
-  #Moverse abajo
-  DFS(x = x, y = y - matrix.data[x][y], matrix = matrix)
+    matrix.increaseSteps()
+    matrix.setVisitedTrue(x, y)
 
-  matrix.setVisitedFalse(x,y)
-  
-  if matrix.data[x][y] == 0:
-    return matrix.getSteps() + 1
+    print(f"{x,y}\n")
 
+    # Moverse a la derecha
+    result = DFS(x + matrix.data[x][y], y, matrix)
+    if result != 0:
+        return result
 
+    # Moverse a la izquierda
+    result = DFS(x - matrix.data[x][y], y, matrix)
+    if result != 0:
+        return result
 
+    # Moverse arriba
+    result = DFS(x, y + matrix.data[x][y], matrix)
+    if result != 0:
+        return result
 
+    # Moverse abajo
+    result = DFS(x, y - matrix.data[x][y], matrix)
+    if result != 0:
+        return result
+
+    matrix.setVisitedFalse(x, y)
 
 #def uniformCost():
